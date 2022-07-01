6c641c3dcebce55f502cf0bf279e7d7fdd41c375
Roberto <carlos12345671@gmail.com>
2022-07-01 10:40:25 -0600

    CAMBIO DE FOOTER

--- views/modules/footer.php
+++ views/modules/footer.php
@@ -1,4 +1,4 @@
   <!-- BEGIN: Footer-->
   <footer class="footer footer-static footer-light footer-shadow">
-      <p class="clearfix mb-0"><span class="float-md-start d-block d-md-inline-block mt-25"> &copy; <?php echo date("Y") ?><a class="ms-25" target="_blank">APPRAVEN</a><span class="d-none d-sm-inline-block"></span></span><span class="float-md-end d-none d-md-block">fFactura electrónica</span></p>
+      <p class="clearfix mb-0"><span class="float-md-start d-block d-md-inline-block mt-25"> &copy; <?php echo date("Y") ?><a class="ms-25" target="_blank">APPRAVEN</a><span class="d-none d-sm-inline-block"></span></span><span class="float-md-end d-none d-md-block">Factura electrónica</span></p>
     </footer>
