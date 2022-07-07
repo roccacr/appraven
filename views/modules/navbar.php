c305c0d162244299b0f59215542e3158347beb61
Roberto <carlos12345671@gmail.com>
2022-07-07 12:22:39 -0600

    cambio

--- views/modules/navbar.php
+++ views/modules/navbar.php
@@ -1,3 +1,34 @@
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
+
+  <title>FACTURA</title>
+  <link rel="apple-touch-icon" href="app-assets/images/ico/apple-icon-120.html">
+ 
+  <!-- BEGIN: Vendor CSS-->
+  <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/vendors.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/charts/apexcharts.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/extensions/toastr.min.css">
+  <!-- END: Vendor CSS-->
+
+  <!-- BEGIN: Theme CSS-->
+  <link rel="stylesheet" type="text/css" href="app-assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/bootstrap-extended.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/colors.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/components.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/themes/dark-layout.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/themes/bordered-layout.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/themes/semi-dark-layout.min.css">
+
+  <!-- BEGIN: Page CSS-->
+  <link rel="stylesheet" type="text/css" href="app-assets/css/core/menu/menu-types/horizontal-menu.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/pages/dashboard-ecommerce.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/plugins/charts/chart-apex.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/plugins/extensions/ext-component-toastr.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/plugins/forms/form-validati
+
 <?php require './variables_globales/variables.php'; ?>
 <!-- BEGIN: Header-->
 <nav class="header-navbar navbar-expand-lg navbar navbar-fixed align-items-center navbar-shadow navbar-brand-center" data-nav="brand-center">
