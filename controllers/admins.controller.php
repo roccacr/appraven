9021d4425cf2be798e161689b17aaae50b044f88
Darío Piedra <dpiedra@roccacr.com>
2025-05-02 11:35:05 -0600

    Refactor AdminsController and CurlController for improved readability and consistency

--- controllers/admins.controller.php
+++ controllers/admins.controller.php
@@ -1,33 +1,42 @@
 <?php
 
-class AdminsController{
+class AdminsController
+{
 
-	/*=============================================
-	Login de administradores
-	=============================================*/
-	public function login(){
-        if(isset($_POST["loginEmail"])){
-	       /*=============================================
-			Validamos la sintaxis de los campos
-			=============================================*/	
+    /*=============================================
+       Login de administradores
+       =============================================*/
+    public function login()
+    {
+        if (isset($_POST["loginEmail"])) {
+            /*=============================================
+                Validamos la sintaxis de los campos
+                =============================================*/
 
-			if(preg_match('/^[.a-zA-Z0-9_]+([.][.a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $_POST["loginEmail"] )){
+            if (preg_match('/^[.a-zA-Z0-9_]+([.][.a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $_POST["loginEmail"])) {
                 require './variables_globales/variables.php';
                 $url = "$tbl_users?login=true&suffix=$suffix_users";
-				$method = "POST";
-				$fields = array(
-					"$email_user_va" => $_POST["loginEmail"],
-					"$password_user_va" => $_POST["loginPassword"]
-				);
-				$response = CurlController::request($url,$method,$fields);
+                $method = "POST";
+                $fields = array(
+                    "$email_user_va" => $_POST["loginEmail"],
+                    "$password_user_va" => $_POST["loginPassword"]
+                );
+                $response = CurlController::request($url, $method, $fields);
+                echo '<div class="alert alert-danger" role="alert">
+                    <h4 class="alert-heading">Prueba con Roberto!!</h4>
+                    <div class="alert-body">
+                        '.$response.'
+                    </div>
+                </div>';
+                return;
                 /*=============================================
-				Validamos que si escriba correctamente los datos
-				=============================================*/	
-				if($response->status == 200){
+                Validamos que si escriba correctamente los datos
+                =============================================*/
+                if ($response->status == 200) {
                     /*=============================================
-					Validamos que si tenga rol administrativo
-					=============================================*/	
-					if($response->results[0]->$estado_user_va !="$estado"){
+                    Validamos que si tenga rol administrativo
+                    =============================================*/
+                    if ($response->results[0]->$estado_user_va != "$estado") {
                         echo '<div class="alert alert-danger" role="alert">
                             <h4 class="alert-heading">Algo salio mal!!</h4>
                             <div class="alert-body">
@@ -35,24 +44,24 @@ class AdminsController{
                             </div>
                         </div>';
                         return;
-					}
+                    }
                     /*=============================================
-					Creamos variable de sesión
-					=============================================*/	
-					$_SESSION["admin"] = $response->results[0];
-					echo '<script>
-					window.location = "'.$_SERVER["REQUEST_URI"].'"
+                    Creamos variable de sesión
+                    =============================================*/
+                    $_SESSION["admin"] = $response->results[0];
+                    echo '<script>
+					window.location = "' . $_SERVER["REQUEST_URI"] . '"
 					</script>';
-                }else{
+                } else {
                     echo '<div class="alert alert-danger" role="alert">
                     <h4 class="alert-heading">Algo salio mal!!</h4>
                     <div class="alert-body">
-                       '.$response->results.'
+                       ' . $response->results . '
                     </div>
                   </div>';
 
                 }
-            }else{
+            } else {
                 echo '<div class="alert alert-danger" role="alert">
                 <h4 class="alert-heading">Campo invalidó</h4>
                 <div class="alert-body">
--- controllers/curl.controller.php
+++ controllers/curl.controller.php
@@ -1,24 +1,26 @@
 <?php
-class CurlController{
+class CurlController
+{
 	/*============================================
-	Peticiones a la API
-	=============================================*/
-	
-	static public function request($url,$method,$fields){
+	   Peticiones a la API
+	   =============================================*/
+
+	static public function request($url, $method, $fields)
+	{
 		$curl = curl_init();
 		curl_setopt_array($curl, array(
-		  CURLOPT_URL => 'https://api-appraven.appsngs.com/'.$url,
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_ENCODING => '',
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 0,
-		  CURLOPT_FOLLOWLOCATION => true,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => $method,
-		  CURLOPT_POSTFIELDS => $fields,
-		  CURLOPT_HTTPHEADER => array(
-		    'Authorization: c5LTA6WPbMwHhEabYu77nN9cn4VcMj'
-		  ),
+			CURLOPT_URL => 'https://api-appraven.appsngs.com/' . $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => $method,
+			CURLOPT_POSTFIELDS => $fields,
+			CURLOPT_HTTPHEADER => array(
+				'Authorization: c5LTA6WPbMwHhEabYu77nN9cn4VcMj'
+			),
 		));
 		$response = curl_exec($curl);
 		curl_close($curl);
