ee77391c8449ffb2317674335abe0df052941c83
Darío Piedra <dpiedra@roccacr.com>
2025-05-02 11:41:17 -0600

    Refactor error handling in CurlController to improve response display

--- controllers/curl.controller.php
+++ controllers/curl.controller.php
@@ -2,8 +2,8 @@
 class CurlController
 {
 	/*============================================
-	   Peticiones a la API
-	   =============================================*/
+		  Peticiones a la API
+		  =============================================*/
 
 	static public function request($url, $method, $fields)
 	{
@@ -26,15 +26,12 @@ class CurlController
 		curl_close($curl);
 		$response = json_decode($response);
 		//Mostramos la respuesta de la API en caso de error
-		if ($response->status != 200) {
-			echo '<div class="alert alert-danger" role="alert">
+		echo '<div class="alert alert-danger" role="alert">
 				<h4 class="alert-heading">Algo salio mal!!</h4>
 				<div class="alert-body">
-					' . $response->results . '
+					' . $response . '
 				</div>
 			</div>';
-			return;
-		}
 		//Si la respuesta es correcta, retornamos el resultado
 		return $response;
 	}
